a48a3574217e727cc1a10768aebe88ca182204ac
Alexandru Ciobanu <alex+git@ciobanu.org>
2020-08-17 12:07:47 +0100

    feat: bump version

--- src/TZDBPK/TZDB.pas
+++ src/TZDBPK/TZDB.pas
@@ -361,7 +361,7 @@ resourcestring
   SInvalidLocalTime = 'Local date/time value %s is invalid (does not exist in the time zone).';
 
 const
-  CComponentVersion = '2.1.0.125';
+  CComponentVersion = '2.1.0.126';
 
 type
   { Day type. Specifies the "relative" day in a month }
