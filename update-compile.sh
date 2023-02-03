10d6507af196ecb8a3bd85a0c2c5e95efaad23bf
Alexandru Ciobanu <alex+git@ciobanu.org>
2023-02-03 11:36:18 +0000

    fix: updated the IANA update script

--- update-compile.sh
+++ update-compile.sh
@@ -100,7 +100,12 @@ fi
 echo "Pulling the latest TZDB database from IANA ..."
 
 rm -rf $REPO/iana_temp 2> /dev/null
-wget -q https://www.iana.org/time-zones/repository/tzdata-latest.tar.gz
+wget -q https://www.iana.org/time-zones
+IANA_GZ_URL=$( cat time-zones.html | sed -nr 's/.*href="(.*tzdata.*\.tar\.gz)".*/\1/p' )
+rm time-zones.html
+
+wget -q "$IANA_GZ_URL"
+mv tzdata*.tar.gz tzdata-latest.tar.gz
 mkdir $REPO/iana_temp
 tar -xf tzdata-latest.tar.gz -C $REPO/iana_temp
 if [ "$?" -ne 0 ]; then
